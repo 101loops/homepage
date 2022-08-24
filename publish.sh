beb4607d10baeb5dee5f919aaf1924293a4d15b1
Stephan Behnke <github@stephanbehnke.com>
2022-08-24 09:45:30 -0700

    publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,7 @@
+export HUGO_SECURITY_FUNCS_GETENV="^HUGO_"
+export HUGO_SECURITY_EXEC_OSENV="(?i)^(PATH|PATHEXT|APPDATA|TMP|TEMP|TERM)$"
+export HUGO_SECURITY_EXEC_ALLOW="^(dart-sass-embedded|go|npx|postcss)$"
+
 ./hugo
 git add .
 git commit -m "publish"
